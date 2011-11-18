292e1dda7c1a5d4b28988cec18186dcfd4bdf07a
louis gueye <louis.gueye@gmail.com>
2011-11-18 17:46:43 +0100

    meta filters and reports support added

--- src/test/java/fr/explorimmo/poc/stories/StoriesRunner.java
+++ src/test/java/fr/explorimmo/poc/stories/StoriesRunner.java
@@ -3,40 +3,76 @@
  */
 package fr.explorimmo.poc.stories;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
-import org.jbehave.core.InjectableEmbedder;
-import org.jbehave.core.annotations.Configure;
-import org.jbehave.core.annotations.UsingEmbedder;
-import org.jbehave.core.annotations.spring.UsingSpring;
-import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.Embeddable;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.i18n.LocalizedKeywords;
 import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.io.StoryFinder;
-import org.jbehave.core.junit.spring.SpringAnnotatedEmbedderRunner;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.model.ExamplesTableFactory;
+import org.jbehave.core.parsers.RegexStoryParser;
+import org.jbehave.core.reporters.CrossReference;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.ParameterConverters;
+import org.jbehave.core.steps.ParameterConverters.DateConverter;
+import org.jbehave.core.steps.ParameterConverters.ExamplesTableConverter;
+import org.jbehave.core.steps.spring.SpringApplicationContextFactory;
+import org.jbehave.core.steps.spring.SpringStepsFactory;
+import org.springframework.context.ApplicationContext;
 
 /**
  * @author louis.gueye@gmail.com
  */
-@RunWith(SpringAnnotatedEmbedderRunner.class)
-@Configure
-@UsingEmbedder(embedder = Embedder.class, generateViewAfterStories = true, ignoreFailureInStories = false, ignoreFailureInView = false, threads = 1)
-@UsingSpring(resources = { "jbehave-context.xml", "stories-context.xml" })
-public class StoriesRunner extends InjectableEmbedder {
+public class StoriesRunner extends JUnitStories {
 
-	/**
-	 * @see org.jbehave.core.Embeddable#run()
-	 */
-	@Test
-	@Override
-	public void run() throws Throwable {
-		injectedEmbedder().runStoriesAsPaths(storyPaths());
-	}
+    private final CrossReference xref = new CrossReference();
 
-	protected List<String> storyPaths() {
-		return new StoryFinder().findPaths(CodeLocations.codeLocationFromClass(this.getClass()).getFile(),
-				Arrays.asList("**/*.story"), null);
-	}
+    @Override
+    public Configuration configuration() {
+        final Class<? extends Embeddable> embeddableClass = this.getClass();
+        final Properties viewResources = new Properties();
+        viewResources.put("decorateNonHtml", "true");
+        // Start from default ParameterConverters instance
+        final ParameterConverters parameterConverters = new ParameterConverters();
+        // factory to allow parameter conversion and loading from external resources (used by StoryParser too)
+        final ExamplesTableFactory examplesTableFactory = new ExamplesTableFactory(new LocalizedKeywords(),
+                new LoadFromClasspath(embeddableClass), parameterConverters);
+        // add custom coverters
+        parameterConverters.addConverters(new DateConverter(new SimpleDateFormat("yyyy-MM-dd")),
+            new ExamplesTableConverter(examplesTableFactory));
+        return new MostUsefulConfiguration()
+                .useStoryLoader(new LoadFromClasspath(embeddableClass))
+                .useStoryParser(new RegexStoryParser(examplesTableFactory))
+                .useStoryReporterBuilder(
+                    new StoryReporterBuilder().withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+                            .withDefaultFormats().withViewResources(viewResources).withFailureTrace(true)
+                            .withFormats(Format.HTML_TEMPLATE).withFailureTraceCompression(true)
+                            .withCrossReference(xref)).useParameterConverters(parameterConverters)
+                .useStepMonitor(xref.getStepMonitor());
+    }
+
+    private ApplicationContext createContext() {
+        return new SpringApplicationContextFactory(this.getClass().getClassLoader(), "jbehave-context.xml",
+                "stories-context.xml").createApplicationContext();
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new SpringStepsFactory(configuration(), createContext());
+    }
+
+    @Override
+    protected List<String> storyPaths() {
+        return new StoryFinder().findPaths(CodeLocations.codeLocationFromClass(this.getClass()).getFile(),
+            Arrays.asList("**/*.story"), null);
+    }
 }
