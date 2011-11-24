e9b3502cd681fcec7edeb1363934765b8ff6c1e8
louis gueye <louis.gueye@gmail.com>
2011-11-24 02:03:36 +0100

    done : content negotiation feature

--- src/test/java/fr/explorimmo/poc/stories/steps/ContentNegotiation.java
+++ src/test/java/fr/explorimmo/poc/stories/steps/ContentNegotiation.java
@@ -39,122 +39,119 @@ import fr.explorimmo.poc.test.TestUtils;
 @Component
 public class ContentNegotiation {
 
-    @Autowired
-    @Qualifier("baseEndPoint")
-    String baseEndPoint;
-    String responseContentType;
-    List<String> resources = new ArrayList<String>();
-    private int responseStatus;
-    private String requestContentType;
-    String lastCreatedResourceURI;
+	@Autowired
+	@Qualifier("baseEndPoint")
+	String			baseEndPoint;
+	String			responseContentType;
+	List<String>	resources	= new ArrayList<String>();
+	private int		responseStatus;
+	private String	requestContentType;
+	String			lastCreatedResourceURI;
 
-    @Then("the response code should be $statusCode")
-    public void expectStatusCode(@Named("statusCode") final int statusCode) {
-        Assert.assertEquals(statusCode, responseStatus);
-    }
+	@Then("the response code should be $statusCode")
+	public void expectStatusCode(@Named("statusCode") final int statusCode) {
+		Assert.assertEquals(statusCode, this.responseStatus);
+	}
 
-    @Then("I should get my newly created resource")
-    public void getResourceAtLocation() {
-        final URI uri = URI.create(lastCreatedResourceURI);
-        final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
-        config.getClasses().add(JacksonJsonProvider.class);
-        final Client jerseyClient = ApacheHttpClient4.create(config);
-        final WebResource webResource = jerseyClient.resource(uri);
-        final Advert advert = webResource.header("Accept", "application/json").get(Advert.class);
-        Assert.assertNotNull(advert);
-    }
+	@Then("I should get my newly created resource")
+	public void getResourceAtLocation() {
+		final URI uri = URI.create(this.lastCreatedResourceURI);
+		final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
+		config.getClasses().add(JacksonJsonProvider.class);
+		final Client jerseyClient = ApacheHttpClient4.create(config);
+		final WebResource webResource = jerseyClient.resource(uri);
+		final Advert advert = webResource.header("Accept", "application/json").get(Advert.class);
+		Assert.assertNotNull(advert);
+	}
 
-    @Then("I should get a successful response")
-    public void getResponse() {
-        Assert.assertNotNull(responseStatus);
-        final int statusCodeFirstDigit = Integer.valueOf(String.valueOf(responseStatus).substring(0, 1));
-        Assert.assertTrue(statusCodeFirstDigit == 2);
-    }
+	@Then("I should get a successful response")
+	public void getResponse() {
+		Assert.assertNotNull(this.responseStatus);
+		final int statusCodeFirstDigit = Integer.valueOf(String.valueOf(this.responseStatus).substring(0, 1));
+		Assert.assertTrue(statusCodeFirstDigit == 2);
+	}
 
-    @Then("I should get an unsuccessful response")
-    public void responseShouldBeUnsuccessful() {
-        Assert.assertNotNull(responseStatus);
-        final int statusCodeFirstDigit = Integer.valueOf(String.valueOf(responseStatus).substring(0, 1));
-        Assert.assertTrue(statusCodeFirstDigit != 2 && statusCodeFirstDigit != 3);
-    }
+	@Then("I should get an unsuccessful response")
+	public void responseShouldBeUnsuccessful() {
+		Assert.assertNotNull(this.responseStatus);
+		final int statusCodeFirstDigit = Integer.valueOf(String.valueOf(this.responseStatus).substring(0, 1));
+		Assert.assertTrue(statusCodeFirstDigit != 2 && statusCodeFirstDigit != 3);
+	}
 
-    @When("I send a create request")
-    public void sendCreateRequest() {
-        final Advert advert = TestUtils.validAdvert();
-        final String path = "/advert";
-        final URI uri = URI.create(baseEndPoint + path);
-        final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
-        config.getClasses().add(JacksonJsonProvider.class);
-        final Client jerseyClient = ApacheHttpClient4.create(config);
-        final WebResource webResource = jerseyClient.resource(uri);
-        jerseyClient.addFilter(new LoggingFilter());
-        final ClientResponse response = webResource.header("Content-Type", requestContentType).post(
-            ClientResponse.class, advert);
-        responseStatus = response.getStatus();
-        if (response.getLocation() != null && StringUtils.isNotEmpty(response.getLocation().toString())) {
-            lastCreatedResourceURI = response.getLocation().toString();
-        }
-    }
+	@When("I send a create request")
+	public void sendCreateRequest() {
+		final Advert advert = TestUtils.validAdvert();
+		final String path = "/advert";
+		final URI uri = URI.create(this.baseEndPoint + path);
+		final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
+		config.getClasses().add(JacksonJsonProvider.class);
+		final Client jerseyClient = ApacheHttpClient4.create(config);
+		final WebResource webResource = jerseyClient.resource(uri);
+		jerseyClient.addFilter(new LoggingFilter());
+		final ClientResponse response = webResource.header("Content-Type", this.requestContentType).post(
+				ClientResponse.class, advert);
+		this.responseStatus = response.getStatus();
+		if (response.getLocation() != null && StringUtils.isNotEmpty(response.getLocation().toString())) this.lastCreatedResourceURI = response
+				.getLocation().toString();
+	}
 
-    @When("I send a search request")
-    public void sendSearchRequest() {
-        final StringBuilder queryBuilder = new StringBuilder();
-        final String path = "/advert/find";
-        final String query = queryBuilder.toString();
-        final URI uri = URI.create(baseEndPoint + path);
-        final String requestContentType = "application/x-www-form-urlencoded";
-        final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
-        config.getClasses().add(JacksonJsonProvider.class);
-        final Client jerseyClient = ApacheHttpClient4.create(config);
-        jerseyClient.addFilter(new LoggingFilter());
-        final ClientResponse response = jerseyClient.resource(uri).accept(MediaType.valueOf(responseContentType))
-                .header("Content-Type", requestContentType).post(ClientResponse.class, query);
-        responseStatus = response.getStatus();
-    }
+	@When("I send a search request")
+	public void sendSearchRequest() {
+		final StringBuilder queryBuilder = new StringBuilder();
+		final String path = "/advert/find";
+		final String query = queryBuilder.toString();
+		final URI uri = URI.create(this.baseEndPoint + path);
+		final String requestContentType = "application/x-www-form-urlencoded";
+		final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
+		config.getClasses().add(JacksonJsonProvider.class);
+		final Client jerseyClient = ApacheHttpClient4.create(config);
+		jerseyClient.addFilter(new LoggingFilter());
+		final ClientResponse response = jerseyClient.resource(uri).accept(MediaType.valueOf(this.responseContentType))
+				.header("Content-Type", requestContentType).post(ClientResponse.class, query);
+		this.responseStatus = response.getStatus();
+	}
 
-    @Given("I send <requestContentType> data")
-    public void setRequestContentType(@Named("requestContentType") final String requestContentType) {
-        this.requestContentType = requestContentType;
-    }
+	@Given("I send <requestContentType> data")
+	public void setRequestContentType(@Named("requestContentType") final String requestContentType) {
+		this.requestContentType = requestContentType;
+	}
 
-    @Given("I receive <responseContentType> data")
-    public void setResponseContentType(@Named("responseContentType") final String responseContentType) {
-        this.responseContentType = responseContentType;
-    }
+	@Given("I receive <responseContentType> data")
+	public void setResponseContentType(@Named("responseContentType") final String responseContentType) {
+		this.responseContentType = responseContentType;
+	}
 
-    @BeforeStory
-    public void setup() {
+	@BeforeStory
+	public void setup() {
 
-        resources.clear();
+		this.resources.clear();
 
-        for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 
-            final Advert advert = TestUtils.validAdvert();
-            final String path = "/advert";
-            final URI uri = URI.create(baseEndPoint + path);
-            final String requestContentType = "application/json";
-            final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
-            final Client jerseyClient = ApacheHttpClient4.create(config);
-            final WebResource webResource = jerseyClient.resource(uri);
-            final ClientResponse response = webResource.header("Content-Type", requestContentType).post(
-                ClientResponse.class, advert);
+			final Advert advert = TestUtils.validAdvert();
+			final String path = "/advert";
+			final URI uri = URI.create(this.baseEndPoint + path);
+			final String requestContentType = "application/json";
+			final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
+			final Client jerseyClient = ApacheHttpClient4.create(config);
+			final WebResource webResource = jerseyClient.resource(uri);
+			final ClientResponse response = webResource.header("Content-Type", requestContentType).post(
+					ClientResponse.class, advert);
 
-            if (response.getLocation() != null) {
-                resources.add(response.getLocation().toString());
-            }
+			if (response.getLocation() != null) this.resources.add(response.getLocation().toString());
 
-        }
+		}
 
-    }
+	}
 
-    @AfterStory
-    public void tearDown() {
-        for (final String resource : resources) {
-            final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
-            final Client jerseyClient = ApacheHttpClient4.create(config);
-            final WebResource webResource = jerseyClient.resource(resource);
-            webResource.delete();
-        }
-    }
+	@AfterStory
+	public void tearDown() {
+		for (final String resource : this.resources) {
+			final DefaultClientConfig config = new DefaultApacheHttpClient4Config();
+			final Client jerseyClient = ApacheHttpClient4.create(config);
+			final WebResource webResource = jerseyClient.resource(resource);
+			webResource.delete();
+		}
+	}
 
 }
